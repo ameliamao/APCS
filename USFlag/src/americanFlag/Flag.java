ab2d3ab56d6fb65a42363e5d08472d4a8a76291d
Amelia <mao.amelia@gmail.com>
2017-09-07 10:34:11 -0700

    adjusted stars

--- USFlag/src/americanFlag/Flag.java
+++ USFlag/src/americanFlag/Flag.java
@@ -72,7 +72,7 @@ public class Flag extends JApplet {
 	 * @param g: graphics object
 	 */
 	public void drawUnion(Graphics g) {
-		final double UNION_OFFSET = 1.32; //offset for stars to be in the correct place on union
+		final double UNION_OFFSET = 1.31; //offset for stars to be in the correct place on union
 		g.setColor(NAVY_BLUE);
 		g.fillRect(0, 0, (int) (D * height), (int) (C * height));
 		Stars star = new Stars(Color.WHITE);
